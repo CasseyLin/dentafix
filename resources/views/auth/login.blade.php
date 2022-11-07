12509efe076973746dc8a4f97ec959318d2e5bda
CasseyLin <92399101+CasseyLin@users.noreply.github.com>
2022-11-07 16:17:24 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<body class="img js-fullheight" style="background-image: url(Images/bg.jpg);">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">

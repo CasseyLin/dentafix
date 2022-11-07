992703d3a1ba4e7d7ee849a30fc4768e563c59a8
CasseyLin <92399101+CasseyLin@users.noreply.github.com>
2022-11-07 16:25:42 +0800

    add chatbot and modify content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+                    Welcome!
                     You are logged in as
                     {{Auth()->user()->name}}
 
@@ -17,3 +18,19 @@
     </div>
 </div>
 @endsection
+
+<script>
+    var botmanWidget = {
+        aboutText: 'Developed with❤️ by Cassey',
+        introMessage: "✋ Hi! I am Cassey, your assistant chatbot from DentaFix~",
+        title:"DentaFix Chatbot",
+        placeholderText: 'Ask Cassey Something...',
+        bubbleBackground: '#FFFFFF',
+        mainColor:'#80E2FF',
+        aboutText:'Developed with ❤️ by Cassey',
+        aboutLink:'https://drive.google.com/file/d/1Pc8pRFxdSyBHazol4EZkqf_WS0raKh1g/view?usp=sharing',
+        bubbleAvatarUrl:'/chatbot/chatbot.jpg'
+    };
+  </script>
+  
+  <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>

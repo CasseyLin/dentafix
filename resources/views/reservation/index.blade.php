2eb9360efe95446b14489813b2e9d49b41261119
CasseyLin <92399101+CasseyLin@users.noreply.github.com>
2022-12-01 14:10:34 +0800

    Add files via upload

---
+++ resources/views/reservation/index.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+
+                <div class="card-header">All appointments({{$patients->count()}})</div>
+
+                 
+                <div class="card-body">
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Image</th>
+                          <th scope="col">Date</th>
+                          <th scope="col">User</th>
+                          <th scope="col">Gender</th>
+                          <th scope="col">Email</th>
+                          <th scope="col">Phone number</th>
+                          <th scope="col">Time</th>
+                          <th scope="col">Dentist</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @forelse($patients as $key=>$patient)
+                        <tr>
+                          <th scope="row">{{$key+1}}</th>
+
+                          <td><img src="/Images/{{$patient->user->image}}" width="80"
+                          style="border-radius:50%;"></td>
+             
+                          <td>{{$patient->date}}</td>
+                          <td>{{$patient->user->name}}</td>
+                          <td>{{$patient->user->gender}}</td>
+                          <td>{{$patient->user->email}}</td>
+                          <td>{{$patient->user->phone_number}}</td>
+                          <td>{{$patient->time}}</td>
+                          <td>{{$patient->dentist->name}}</td>
+
+                        </tr>
+                        @empty
+                        <td>No appointment has been made on this day.</td>
+                        @endforelse
+                       
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
